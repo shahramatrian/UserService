55572ff64b8e544b422a40ce2bfcdc82ab6e60fc
Shahram Atrian <shahramatrian@gmail.com>
2023-09-06 12:35:17 -0700

    Removed commented out imports

--- person-service/src/main/java/com/shahramatrian/controller/PersonalInfoController.java
+++ person-service/src/main/java/com/shahramatrian/controller/PersonalInfoController.java
@@ -1,10 +1,8 @@
 package com.shahramatrian.controller;
 
-// import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
